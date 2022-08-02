feb4d3ec61b0b8ac0022ac0b19b6b897b0f7d8c2
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2022-08-02 22:18:21 +0200

    Handle edges in BrentMin.

--- src/optimize/univariate/brentmin.jl
+++ src/optimize/univariate/brentmin.jl
@@ -1,9 +1,10 @@
 struct BrentMin{T}
     division::T
-    detect_flatness::Bool
+    evaluate_bounds::Bool
+    tiebreak_down::Bool
 end
-BrentMin(; division = (3 - sqrt(5)) / 2, detect_flatness = false) =
-    BrentMin(division, detect_flatness)
+BrentMin(; division = (3 - sqrt(5)) / 2, evaluate_bounds = true, tiebreak_down = true) =
+    BrentMin(division, evaluate_bounds, tiebreak_down)
 
 function solve(
     problem::OptimizationProblem,
@@ -23,10 +24,42 @@ function _solve(prob, bm::BrentMin, options)
 
     e = d = 0 * x
 
-    f0 = fv = fw = fx = value(prob, x)
+    # The following variable names are explained on (p. 73)
+    #   - x is the point with the lowest objective value observed
+    #       with a value of fx. It is possible that more than one value
+    #       of the input has returned an objective equal to fx and
+    #       in that case the (x, fx) pair is the most recent such pair.
+    #   - w is the point with the next lowest objective value of fw
+    #   - v is the previous value of w with objective fv
+    #   - u is the last point at which the objective was evaluated
+    #   - f0 is the first value observed
+    fv = fw = fx = value(prob, x)
+
+    # We modify the algorith slightly here, and evaluate the objetive
+    # at the bounds initially. If this is not what the user wants, they
+    # should set `evaluate_bounds = false`.
+    if bm.evaluate_bounds
+        fa = value(prob, a)
+        fb = value(prob, b)
+        if fa < fx || fb < fx
+            if fa < fb
+                x = a
+                fx = fa
+            else
+                x = b
+                fx = fb
+            end
+        end
+    end
+
+    # Set f0 to the (corrected) initial fx
+    f0 = fx
+
     p = q = r = 0 * x
     brent_iter = 0
+
     while brent_iter < options.maxiter
+
         brent_iter += 1
         m = (a + b) / 2
         tol = eps(T) * abs(x) + t
@@ -70,6 +103,7 @@ function _solve(prob, bm::BrentMin, options)
             end
 
             fu = value(prob, u)
+
             if fu <= fx
                 if u < x
                     b = x
@@ -102,6 +136,23 @@ function _solve(prob, bm::BrentMin, options)
             break
         end
     end
-
+    if bm.evaluate_bounds
+        # Even if we stopped it can still be the case that x is not a or b even if they're the lowest.
+        # We check if fx is indeed the lowest point. If not, we return fa or fb. We tie break to exact
+        # bound.
+        if fx >= fa
+            if fx >= fb
+                if bm.tiebreak_down
+                    x = a
+                else
+                    x = b
+                end
+            else
+                x = a
+            end
+        elseif fx >= fb
+            x = b
+        end
+    end
     return ConvergenceInfo(bm, (;x, f0, minimum=fx, time=time()-time0, iter=brent_iter), options)
 end
