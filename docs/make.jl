f5778f6d8ce195d74972f15869fdf3fed3ef7d88
Alex Arslan <ararslan@comcast.net>
2023-04-15 13:12:59 -0700

    Update CI and Documenter setup
    CI changes:
    - Update the `actions/checkout` version
    - Use `julia-actions/cache` to simplify the `actions/cache` setup
    - Submit coverage information to Codecov
    - Use `julia-actions` actions for documentation building/deployment
    - Remove the now-unused `.travis.yml`
    
    Documentation changes:
    - Use the DocumenterMarkdown package and fix the MkDocs-based deployment
      based on the Documenter manual. (This is why the current docs use the
      default Documenter theme rather than the specified MkDocs Windmill
      theme.)
    - Remove the `docs/build/` directory, which shouldn't be checked in
    - Fix the repo URL in `mkdocs.yml`
    
    Other changes:
    - Add a few more standard things to `.gitignore`, including
      `docs/build/`
    - Update the CI and documentation badges in the README

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,15 @@
-using Documenter, NLSolvers
+using Documenter, DocumenterMarkdown, NLSolvers
 
 makedocs(
     doctest = false,
     sitename = "NLSolvers.jl",
-    pages = ["index.md", "optimization.md",]
+    pages = ["index.md", "optimization.md",],
+    format = Markdown(),
 )
 
 deploydocs(
     deps = Deps.pip("pygments", "mkdocs", "python-markdown-math", "mkdocs-windmill"),
     repo = "github.com/JuliaNLSolvers/NLSolvers.jl.git",
+    make = () -> run(`mkdocs build`),
+    target = "site",
 )
