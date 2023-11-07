e9cc27adcd5ac34b08b2988368bc64045796fab7
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2023-11-07 09:15:03 +0100

    Update preconditioning.jl

--- test/optimize/preconditioning.jl
+++ test/optimize/preconditioning.jl
@@ -45,9 +45,10 @@ using Test
                 N == 10 &&
                 optimizer(nothing).linesearcher isa HZAW &&
                 optimizer(nothing).scheme isa LBFGS
-            )
+                || N ∈ (50,100) && optimizer(nothing).linesearcher isa Backtracking && optimizer(nothing).scheme isa ConjugateGradient)
                 @test iter[end-1] >= iter[end]
             end
         end
     end
 end
+
